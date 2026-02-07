3c45e24849a0a7047d159d8043766b2bf46e834d
Guru Charan <nalabologurucharan12-cpu@users.noreply.github.com>
2026-02-07 10:45:13 +0530

    Added routes for login and product pages

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Home route
+# Home page
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # To get data from login form
+        username = request.form.get("username")
+        password = request.form.get("password")
+        print("Username:", username)
+        print("Password:", password)
+
+        return "Login Successful (temporary response)"
+
+    return render_template("login.html")
+
+
+# Products page
+@app.route("/products")
+def products():
+    return render_template("products.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
